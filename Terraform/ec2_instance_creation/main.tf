dd0a9c93a2b3e90dd3dafe71adfa2e7b0ebcc3e8
naveenprofile <148776387+naveenprofile@users.noreply.github.com>
2024-04-29 13:51:34 +0000

    key removed

--- Terraform/ec2_instance_creation/main.tf
+++ Terraform/ec2_instance_creation/main.tf
@@ -4,7 +4,6 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami = var.ami_value
-  key_name = var.key_name_value
   instance_type = var.instance_type_value
   subnet_id = var.subnet_id_value
 }
--- Terraform/ec2_instance_creation/variable.tf
+++ Terraform/ec2_instance_creation/variable.tf
@@ -5,12 +5,6 @@ variable "ami_value" {
 variable "instance_type_value" {
     description = "value is for instance_type"
 }
-
-variable "key_name_value" {
-    description = "value is for key pair"
-  }
-
 variable "subnet_id_value" {
-    description = "value is for subnet"
-  
+    description = "value is for subnet"  
 }
