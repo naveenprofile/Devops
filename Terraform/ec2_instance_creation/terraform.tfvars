4727d72849b5cff2b6db2cb6c6ac2c74e8e8b6ae
naveenprofile <naveenprofile.fis@gmail.com>
2024-04-29 10:49:38 +0530

    push all files to repo

---
+++ Terraform/ec2_instance_creation/terraform.tfvars
@@ -0,0 +1,4 @@
+ami_value="ami-0f58b397bc5c1f2e8"
+instance_type_value="t2.micro"
+key_name_value="aws.pem"
+subnet_id_value="subnet-0dec73d99e57a0707"
---
+++ Terraform/ec2_instance_creation/variable.tf
@@ -0,0 +1,16 @@
+variable "ami_value" { 
+    description = "value is for ami" 
+}
+
+variable "instance_type_value" {
+    description = "value is for instance_type"
+}
+
+variable "key_name_value" {
+    description = "value is for key pair"
+  }
+
+variable "subnet_id_value" {
+    description = "value is for subnet"
+  
+}
